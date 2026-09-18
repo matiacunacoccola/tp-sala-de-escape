9a1457fa3d1305fce906a07e2ca7a54396de64b4
Tre <matiacunacoccola@gmail.com>
2026-09-18 18:51:20 -0300

    cambios

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,7 +1,5 @@
-﻿let intentos=0;
-let palabra = document.getElementById("palabra").value;
+﻿let palabra = document.getElementById("palabra").value;
 let palabraOculta=[];
-let gano=false;
 
 for(let i=0; i<palabra.length; i++)
 {
@@ -19,16 +17,6 @@ document.getElementById("palabraOculta").innerHTML=texto;
 
 function arriesgarLetra()
 {
-  if(gano==true)
-  {
-     return;
-  }
-
-  if(intentos >=10)
-  {
-     return;
-  }
-
   let letra=document.getElementById("letra").value.toUpperCase();
 
   if(letra.length!=1)
@@ -37,43 +25,29 @@ function arriesgarLetra()
      return;
   }
 
-  let encontrada=false;
-
   for(let i=0; i<palabra.length;i++)
   {
     if(palabra[i]==letra){
         palabraOculta[i]=letra;
-        encontrada=true;
      }
   }
 
   let texto = "";
   let quedanGuiones=false;
-
   for(let i=0; i<palabraOculta.length; i++)
   {
     texto += palabraOculta[i] + " ";
 
-    if(palabraOculta[i] =="_"){
+    if(palabraOculta[i] =="_")
+   {
      quedanGuiones=true;
     }
   }
   document.getElementById("palabraOculta").innerHTML = texto;
 
-  if(quedanGuiones==false){
-     document.getElementById("mensaje").innerHTML="Ganaste";
-     gano=true;
-     document.getElementById("continuar").style.display="block";
-     return;
-  }
-  if(encontrada==false)
- {
-     intentos ++;
-  }
-  document.getElementById("intentos").innerHTML= 10- intentos;
-  if(intentos >= 10)
- {
-     document.getElementById("mensaje").innerHTML="Perdiste. La palabra era: " + palabra;
+  if(quedanGuiones==false)
+{
+     window.location.href = "/Juego/ResolverAhorcado";
      return;
   }
 }
